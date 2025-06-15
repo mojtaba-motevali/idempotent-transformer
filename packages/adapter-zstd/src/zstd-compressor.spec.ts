aaea78dd07326fae90ff39fb1f159396c85beea9
Mojtaba Motevali <m.motevali1376@gmail.com>
2025-06-15 20:07:35 +0200

    feat(core): Fix unit tests

--- packages/adapter-zstd/src/zstd-compressor.spec.ts
+++ packages/adapter-zstd/src/zstd-compressor.spec.ts
@@ -3,10 +3,10 @@ import { ZstdCompressor } from './zstd-compressor';
 describe('ZstdCompressor', () => {
   it('should compress and decompress data', async () => {
     const compressor = new ZstdCompressor();
-    const data = new Uint8Array([1, 2, 3, 4, 5]);
-    const compressed = await compressor.compress(data);
+    const data = JSON.stringify([1, 2, 3, 4, 5]);
+    const compressed = await compressor.compress(Buffer.from(data));
     const decompressed = await compressor.decompress(compressed);
-    expect(decompressed).toEqual(data);
+    expect(JSON.parse(decompressed.toString())).toEqual(JSON.parse(data));
   });
 
   it('should detect compressed data', () => {
