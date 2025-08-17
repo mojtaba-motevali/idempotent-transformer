4082583c5bf85501ec7a77bfb052426cbf0042a1
Mojtaba <41769202+mojtaba-motevali@users.noreply.github.com>
2025-08-17 23:49:11 +0200

    Add dynamic thread support, and increase snapshot delay

--- server/src/main.rs
+++ server/src/main.rs
@@ -30,74 +30,92 @@ async fn shutdown_gracefully(
     }
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+fn main() -> Result<(), Box<dyn Error>> {
     dotenvy::dotenv().ok();
-    let server_node = Server {
-        id: var("NODE_ID")
-            .expect("NODE_ID is not set")
-            .parse::<u64>()
-            .unwrap(),
-        addr_api: var("ADDR_API").expect("ADDR_API is not set").to_string(),
-        addr_raft: var("ADDR_RAFT").expect("ADDR_RAFT is not set").to_string(),
-    };
+    let total_threads = var("TOKIO_WORKER_THREADS")
+        .expect("TOKIO_WORKER_THREADS is not set")
+        .parse::<usize>()
+        .unwrap();
+    let stack_size = var("TOKIO_WORKER_STACK_SIZE")
+        .expect("TOKIO_WORKER_STACK_SIZE is not set")
+        .parse::<usize>()
+        .unwrap();
+    println!("Starting server");
+    // Configure Tokio runtime with custom stack size and thread count
+    let runtime = tokio::runtime::Builder::new_multi_thread()
+        .worker_threads(total_threads)
+        .thread_stack_size(stack_size)
+        .enable_all()
+        .build()
+        .unwrap();
 
-    let rpc_addr = var("RPC_ADDR").expect("RPC_ADDR is not set").to_string();
-    let nodes = var("NODES")
-        .expect("NODES is not set")
-        .split(",")
-        .map(|s| Server::parse(s).unwrap())
-        .collect::<Vec<Server>>();
-    let data_dir = var("DATA_DIR").expect("DATA_DIR is not set").to_string();
+    runtime.block_on(async {
+        let server_node = Server {
+            id: var("NODE_ID")
+                .expect("NODE_ID is not set")
+                .parse::<u64>()
+                .unwrap(),
+            addr_api: var("ADDR_API").expect("ADDR_API is not set").to_string(),
+            addr_raft: var("ADDR_RAFT").expect("ADDR_RAFT is not set").to_string(),
+        };
 
-    tracing_subscriber::fmt()
-        .with_target(true)
-        .with_level(true)
-        .with_env_filter(EnvFilter::from("info"))
-        .init();
+        let rpc_addr = var("RPC_ADDR").expect("RPC_ADDR is not set").to_string();
+        let nodes = var("NODES")
+            .expect("NODES is not set")
+            .split(",")
+            .map(|s| Server::parse(s).unwrap())
+            .collect::<Vec<Server>>();
+        let data_dir = var("DATA_DIR").expect("DATA_DIR is not set").to_string();
 
-    let client = get_client(server_node.clone(), data_dir, nodes)
-        .await
-        .map_err(|e| Box::new(e) as Box<dyn Error>)?;
-    let mut shutdown_handle = client.shutdown_handle()?;
+        tracing_subscriber::fmt()
+            .with_target(true)
+            .with_level(true)
+            .with_env_filter(EnvFilter::from("info"))
+            .init();
 
-    init_tables(&client)
-        .await
-        .map_err(|e| Box::new(e) as Box<dyn Error>)?;
-    let scheduler = clean_up_expired_workflows(&client).await?;
+        let client = get_client(server_node.clone(), data_dir, nodes)
+            .await
+            .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+        let mut shutdown_handle = client.shutdown_handle()?;
 
-    let mut sigterm = signal::unix::signal(signal::unix::SignalKind::terminate()).unwrap();
-    let mut sigint = signal::unix::signal(signal::unix::SignalKind::interrupt()).unwrap();
+        init_tables(&client)
+            .await
+            .map_err(|e| Box::new(e) as Box<dyn Error>)?;
+        let scheduler = clean_up_expired_workflows(&client).await?;
 
-    tokio::select! {
-        result = start_server(&rpc_addr, &client) => {
-            match result {
-                Ok(_) => {
-                    error!("Server stopped unexpectedly");
-                    shutdown_gracefully(scheduler, shutdown_handle.wait()).await;
-                    Ok(())
-                }
-                Err(e) => {
-                    error!("Error starting server: {}", e);
-                    shutdown_gracefully(scheduler, shutdown_handle.wait()).await;
-                    Err(e)
+        let mut sigterm = signal::unix::signal(signal::unix::SignalKind::terminate()).unwrap();
+        let mut sigint = signal::unix::signal(signal::unix::SignalKind::interrupt()).unwrap();
+
+        tokio::select! {
+            result = start_server(&rpc_addr, &client) => {
+                match result {
+                    Ok(_) => {
+                        error!("Server stopped unexpectedly");
+                        shutdown_gracefully(scheduler, shutdown_handle.wait()).await;
+                        Ok(())
+                    }
+                    Err(e) => {
+                        error!("Error starting server: {}", e);
+                        shutdown_gracefully(scheduler, shutdown_handle.wait()).await;
+                        Err(e)
+                    }
                 }
             }
+            _ = signal::ctrl_c() => {
+                error!("Received Ctrl+C, initiating graceful shutdown...");
+                shutdown_gracefully(scheduler, shutdown_handle.wait()).await;
+                Ok(())
+            }
+            _ = sigterm.recv() => {
+                error!("Received SIGTERM, initiating graceful shutdown...");
+                shutdown_gracefully(scheduler, shutdown_handle.wait()).await;
+                Ok(())
+            }
+            _ = sigint.recv() => {
+                error!("Received SIGINT, initiating graceful shutdown...");
+                shutdown_gracefully(scheduler, shutdown_handle.wait()).await;
+                Ok(())
+            }
         }
-        _ = signal::ctrl_c() => {
-            error!("Received Ctrl+C, initiating graceful shutdown...");
-            shutdown_gracefully(scheduler, shutdown_handle.wait()).await;
-            Ok(())
-        }
-        _ = sigterm.recv() => {
-            error!("Received SIGTERM, initiating graceful shutdown...");
-            shutdown_gracefully(scheduler, shutdown_handle.wait()).await;
-            Ok(())
-        }
-        _ = sigint.recv() => {
-            error!("Received SIGINT, initiating graceful shutdown...");
-            shutdown_gracefully(scheduler, shutdown_handle.wait()).await;
-            Ok(())
-        }
-    }
+    })
 }
